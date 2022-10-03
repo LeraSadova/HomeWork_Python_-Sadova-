ca0a5a4f2371a15ca534717bff9eb03776ecd9a4
Садова Валерия Андреевна <SadovaVA@vseinstrumenti.ru>
2022-10-03 14:53:21 +0300

    задача 5 домашки 4

--- HomeWork_04/ex5/ex5.py
+++ HomeWork_04/ex5/ex5.py
@@ -1,4 +1,27 @@
 # Даны два файла, в каждом из которых находится запись многочлена. 
 # Задача - сформировать файл, содержащий сумму многочленов.
 
-#не решила
+#не решила как просите. Напишу алгоритм просто для сложения чисел из двух файлов
+
+from turtle import st
+
+
+numbers1 = ['100']
+data = open ('HW4_ex5_01.txt', 'a')
+data.writelines(numbers1)
+data.close()
+
+numbers2 = ['250']
+data = open ('HW4_ex5_02.txt', 'a')
+data.writelines(numbers2)
+data.close()
+
+# пыталась я в инт перевестич исла из файла
+# num1=int(numbers1)
+# num2=int(numbers2)
+# num3=num1+num2
+
+numbers3 = numbers1+numbers2
+data = open ('HW4_ex5_03.txt', 'a')
+data.writelines(numbers3)
+data.close()
